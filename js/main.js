9a85b8e5ed9177afe8897e3486aa98b6dd99cdc7
Oleh <olegkandaskalov@gmail.com>
2022-12-04 14:09:09 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const storeName = "Eko";
+const storeDescription = {
+    budget: 10000,
+    employees: ['Janna', 'Lala', 'Zaza'],
+    products: {
+        'bread': 5,
+        'juice': 3,
+
+    },
+    open: true,    
+};
