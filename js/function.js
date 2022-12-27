c961bbd035b1ce0f97fe5afb01f1c35519d614ce
Oleh <olegkandaskalov@gmail.com>
2022-12-27 14:34:29 +0200

    create function for show DB and add favorite geners. Add inde.html and test file .js(function.js)

---
+++ js/function.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const logg = "Hello world";
+
+console.log(logg.slice(6));
+console.log(parseFloat('12.2px'));
--- js/main.js
+++ js/main.js
@@ -1,13 +1,68 @@
-"use strict";
+'use strict';
 
-const storeName = "Eko";
-const storeDescription = {
-    budget: 10000,
-    employees: ['Janna', 'Lala', 'Zaza'],
-    products: {
-        'bread': 5,
-        'juice': 3,
+let numberOfFilms;
 
-    },
-    open: true,    
+function start() {
+    numberOfFilms = +prompt('How much film you see?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How much film you see?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false,
 };
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Last watched movie?', ''),
+              b = prompt('Rate this film', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+    
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Ви мало дивитесь фільми');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.movies < 30){
+        console.log('Ви класичний глядач');
+    } else if (personalMovieDB.count >= 30){
+        console.log('Ви кіноман');
+    } else {
+        console.log('Помилка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGeners() {
+    for (let i = 0; i < 3; i++){
+        personalMovieDB.geners.push(prompt(`Ваш улюблений жанр под номером ${i+1}`, ''));
+    }
+}
+
+writeYourGeners();
